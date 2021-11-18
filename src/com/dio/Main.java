12fdb9df44e06ab2773128d2e4dcf598e210278a
mario signorelli <signorelii2308@gmail.com>
2021-11-18 18:36:09 -0300

    novas atualizaçao

---
+++ src/com/dio/Main.java
@@ -0,0 +1,22 @@
+package com.dio;
+
+import com.dio.model.gatos;
+
+public class Main {
+
+	public static void main(String[] args) {
+		livros = new livros();
+		Gato gato = new gatos();
+	/*	int a = 2;
+		int b = 3;
+		
+		System.out.println("Hello word people a" );
+		System.out.println("a  soma de  a + b é =" +(a+ b)); */
+
+	}
+
+}
+ class Livros{
+	  private String nome;
+	  private String npg;
+ }
